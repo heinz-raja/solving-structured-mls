32f691cac266b31b2e62fb2f5f5140550af1b026
heinz-raja <heinzraja@gmail.com>
2023-12-01 13:07:14 +0000

    Update 88 files
    - /circulant.m
    - /btdgen.m
    - /cpd.m
    - /cpd_aels.m
    - /cpd_als.m
    - /cpd_core.m
    - /cpd_els.m
    - /cpd_eps.m
    - /cpd_gevd.m
    - /cpd_lsb.m
    - /cpd_minf.m
    - /cpd_nls.m
    - /cpd_rnd.m
    - /cpd3_sd.m
    - /cpd3_sgsd.m
    - /cpdgen.m
    - /cum3.m
    - /cum4.m
    - /deriv.m
    - /dotk.m
    - /frob.m
    - /gramschmidt.m
    - /gap.m
    - /housegen.m
    - /hqrd.m
    - /isSqMatrixDiagonal.asv
    - /isSqMatrixDiagonal.m
    - /isTensorDiagonal.asv
    - /isTensorDiagonal.m
    - /getTensorSuperDiag.m
    - /kmeans.m
    - /kr.m
    - /kron.m
    - /ll1.m
    - /lmlra.m
    - /lmlra_hooi.m
    - /LSR1_proj.m
    - /lmlra3_dgn.m
    - /lmlra3_rtr.m
    - /lmlraerr.m
    - /lmlragen.m
    - /ndiag.m
    - /ls_mt.m
    - /mat2tens.m
    - /minf_lbfgs.m
    - /minf_lbfgsdl.m
    - /minf_ncg.m
    - /minf_sr1cgs.m
    - /mlrank.m
    - /mlrankest.m
    - /mlsvd.m
    - /mpcg.m
    - /nls_gncgs.m
    - /nls_gndl.m
    - /relative_orthogonalize.m
    - /nls_lm.m
    - /nlsb_gndl.m
    - /noisy.m
    - /polymin.m
    - /polymin2.m
    - /polysol2.m
    - /polyval2.m
    - /solve_circ.asv
    - /rankest.m
    - /solve_circ.m
    - /solve_part_diag.m
    - /solve_part_diag.asv
    - /solve_super_diag.m
    - /ratmin.m
    - /ratmin2.m
    - /scov.m
    - /slice3.m
    - /spy3.m
    - /test_getTensorSuperDiag.mlx
    - /tensorlab_2016-03-28.zip
    - /test_isTensorDiagonal.mlx
    - /test_LSR1_proj.mlx
    - /test_solv_circ.mlx
    - /test_solve_part_diag.mlx
    - /try_solve_syspart_diag.mlx
    - /sumk.m
    - /surf3.m
    - /tens2mat.m
    - /tens2vec.m
    - /tmprod.m
    - /vec2tens.m
    - /voxel3.m
    - /btd_rnd.m

---
+++ LSR1_proj.m
@@ -0,0 +1,38 @@
+function x = LSR1_proj(T,b)
+%   [x] = LSR1_proj(T,b) where T is a tensor of
+%   order 3 and b is the right-hand side in the equation:
+%      T.2x.3x = b
+%   
+% 
+%   Authors: Heinz Doss         (h.doss@student.maastrichtuniversity.nl)
+%            Martijn Bousse     (m.bousse@maastrichtuniversity.nl)
+%
+% Version History:
+% - 2023/11/15   HD      Initial implementation
+    
+    %input check
+    %Assuming order 3
+    sz = size(T);
+    if(size(sz)<3)
+        error('LSR1_proj:T','not a tensor')
+    end
+    if(sz(2)~=sz(3))
+        error('LSR1_proj:T','2nd and third modes must be equal')
+    end
+    if(sz(1)<(sz(2)*2))
+        error('LSR1_proj:T','3rd mode must be greater than 2*1st mode')
+    end
+    if(sz(1)~=size(b,1))
+        error('LSR1_proj:T','dimensions of b and T do not match')
+    end
+    A = tens2mat(T,1);            
+    V = orth(A(:,sz(2)+1:end));
+    A_tilde = (eye(size(V,1))-V*V')*A;
+    A_tilde = A_tilde(:,1:sz(2));
+    x_tilde = A_tilde\b;
+    x = x_tilde;
+%     x = x./x(1,1);
+%     x = zeros(sz(2),1);
+%     x(1,1) = x_tilde(1,1)^(1/sz(2));
+    
+end
---
+++ solve_super_diag.m
@@ -0,0 +1,27 @@
+function [x] = solve_super_diag(D,N,b)
+%UNTITLED Summary of this function goes here
+%   [x] = SOLVE_SUPER_DIAG(D,N,b) where D is an array containing the
+%   diagonal of an Nth-order tensor and b is the right-hand side in the
+%   equation:
+%      T.2x.3x = b
+%   
+% 
+%   Authors: Heinz Doss         (h.doss@student.maastrichtuniversity.nl)
+%            Martijn Bousse     (m.bousse@maastrichtuniversity.nl)
+%
+% Version History:
+% - 2023/10/25   HD      Initial implementation
+
+    % input check
+    % if N is odd, then the b/d should be positive (check complex)
+    % if N is even, then ...
+
+    % pre-processing
+    sz = size(D,2);
+    
+    % computation
+    x = zeros(1,sz);
+    for i=1:sz
+        x(i) = (b(i)/D(i))^(1/(N-1));
+    end
+end
